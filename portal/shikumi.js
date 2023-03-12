540e9edf6aaa3be1351b9e8df66b9f9904bbba98
cat73220 <cat73220@users.noreply.github.com>
2023-03-12 08:40:09 +0000

    Deploy blog and portal from cc95062497e6f41cd1118e7b9ce67762a53b60ae
    Masatoshi Watanabe: 20230312 : portal : 最初の画像の高さを画面高さに自動調整 : 開始

---
+++ portal/shikumi.js
@@ -0,0 +1,8 @@
+console.log('shikumi.js here');
+const hdr = document.getElementsByTagName('header');
+hdr[0].width = window.innerWidth;
+hdr[0].height = window.innerHeight;
+hdr[0].height = 1080;
+console.log('shikumi : hdr :',hdr);
+console.log(`shikumi : window inner : W:${window.innerWidth}, H:${window.innerHeight}`);
+
